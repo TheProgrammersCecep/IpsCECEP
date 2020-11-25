336c9a389b55b1a6f6b0b2efb6df13f8d20a9f73
Sebastian Salinas Valencia <74941802+SebasSalinas20@users.noreply.github.com>
2020-11-24 21:44:18 -0500

    Update nav-admin.php

--- php/Acceso/nav-admin.php
+++ php/Acceso/nav-admin.php
@@ -6,8 +6,8 @@
   <div id="collapseUsuarios" class="collapse" aria-labelledby="headingUsuarios" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
       <a class="collapse-item" href="#">Afiliados</a>
-      <a class="collapse-item" href="#">Empleados</a>
-      <a class="collapse-item" href="#">Usuarios Comunes</a>
+      <a class="collapse-item" href="php/Modulos/Usuarios/empleados.php">Empleados</a>
+      <a class="collapse-item" href="php/Modulos/Usuarios/Medico/medicos.php">Medicos</a>
     </div>
   </div>
 </li>
@@ -21,9 +21,9 @@
   </a>
   <div id="collapseLocalidad" class="collapse" aria-labelledby="headingLocalidad" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <a class="collapse-item" href="#">Sedes</a>
-      <a class="collapse-item" href="#">Ciudades</a>
-      <a class="collapse-item" href="#">Paises</a>
+      <a class="collapse-item" href="php/Modulos/Localizacion/sede.php">Sedes</a>
+      <a class="collapse-item" href="php/Modulos/Localizacion/ciudad.php">Ciudades</a>
+      <a class="collapse-item" href="php/Modulos/Localizacion/pais.php">Paises</a>
     </div>
   </div>
 </li>
