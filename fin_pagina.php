f73b27ccbd3f8e12678ba32dc267fc7889abdbc4
Sebastian Salinas Valencia <74941802+SebasSalinas20@users.noreply.github.com>
2020-11-24 21:31:57 -0500

    Create fin_pagina.php

---
+++ fin_pagina.php
@@ -0,0 +1,19 @@
+      <!-- Pie de pagina -->
+      <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; IPS CECEP 2020</span>
+            </div>
+          </div>
+        </footer>
+        <!-- Final - Pie de pagina -->
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Boton de moverse arriba-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
