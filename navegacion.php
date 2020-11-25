3cfef9d3c6b6e1e8d46d5c1ccdffd6d6e233458f
Sebastian Salinas Valencia <74941802+SebasSalinas20@users.noreply.github.com>
2020-11-24 22:07:57 -0500

    Update navegacion.php

--- navegacion.php
+++ navegacion.php
@@ -10,7 +10,7 @@
         <div class="sidebar-brand-text mx-3">IPS GESTION</div>
       </a>
 
-      <!-- Divider -->
+      <!-- Divider navegacion -->
       <hr class="sidebar-divider my-0">
 
       <!-- Inicio -->
