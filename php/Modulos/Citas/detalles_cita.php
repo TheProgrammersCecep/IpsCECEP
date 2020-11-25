b095d56a690f28650c3ed0091a106415af409ee9
MarilyG9909 <giotorrez84@gmail.com>
2020-11-24 22:37:03 -0500

    Create detalles_cita.php

---
+++ php/Modulos/Citas/detalles_cita.php
@@ -0,0 +1,32 @@
+<div class="modal fade" id="detalles" tabindex="-1" >
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Detalles cita</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <label for="validationTooltip04">Paciente</label>
+      <label class="form-control" id="dnombre"></label>
+
+      <label for="validationTooltip04">Fecha</label>
+      <label class="form-control" id="dfecha"></label>
+
+      <label for="validationTooltip04">Hora</label>
+      <label class="form-control" id="dhora"></label>
+
+      <label for="validationTooltip04">Sede</label>
+      <label class="form-control" id="dsede"></label>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Imprimir</button>
+      </div>
+    </div>
+  </div>
+</div>
