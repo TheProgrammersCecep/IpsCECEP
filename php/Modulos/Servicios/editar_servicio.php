afef823cf4b37083eb08c2911869d2671b4641e2
AndresBlandonW <74189991+AndresBlandonW@users.noreply.github.com>
2020-11-24 22:38:37 -0500

    Add files via upload

---
+++ php/Modulos/Servicios/editar_servicio.php
@@ -0,0 +1,33 @@
+
+<form role="form"  id="feditarp">
+  <div class="modal fade" id="editar" tabindex="-1" >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Editar Servicio</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+
+        <label for="validationTooltip04">Codigo</label>
+        <input type="text" class="form-control" id="scodigo" name="scodigo" value = "" readonly="true"/>
+        <br>
+        <label for="validationTooltip04">Servicio</label>
+        <input type="text" class="form-control" id="sservicio" name="sservicio"/>
+        <br>
+        <label for="validationTooltip04">Descripcion</label>
+        <input type="text" class="form-control" id="sdesc" name="sdesc"/>
+        <br>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <button type="button" id="actualizar" class="btn btn-primary" data-dismiss="modal">Guardar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <input type="hidden" id="editar" value="editar" name="accion"/>
+</form>
