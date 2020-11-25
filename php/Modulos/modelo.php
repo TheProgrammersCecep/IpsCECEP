3c925a4a6dbed45ba7f4244e6819b2d0458a3c7a
MarilyG9909 <giotorrez84@gmail.com>
2020-11-24 22:36:46 -0500

    Create modelo.php

---
+++ php/Modulos/modelo.php
@@ -0,0 +1,16 @@
+<?php include_once ("../sesiones.php"); ?>
+<link rel="STYLESHEET" href="./css/custom-style.css">
+
+<div class="container-fluid">
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1>TITULO PAGINA</h1>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.2/sweetalert2.all.js"></script>
+
+    <!--TODO EL CONTENIDO AQUI-->
+
+
+
+</div>
